3ae61fc0a7d867f4705605767c11df33cdf7e0a3
Mr-Jitesh9170 <mr.jiteshpandey9170@gmail.com>
2025-09-05 17:55:04 +0530

    changes fixed issue in room joining

--- client/src/pages/ticTacToeRoomCreation.jsx
+++ client/src/pages/ticTacToeRoomCreation.jsx
@@ -7,7 +7,6 @@ import { MdOutlineAdsClick } from "react-icons/md";
 import JoinedPlayers from "./joinedPlayers";
 import { io } from "socket.io-client";
 
-
 const socket = io("http://localhost:8080")
 const today = new Date()
 const YourId = `userId${today.getFullYear() + today.getMilliseconds() + today.getSeconds()}`
@@ -18,8 +17,9 @@ export const TicTacToeRoomCreation = () => {
     const [joinedPlayers, setJoinedPlayers] = useState([])
     const [inputs, setIputs] = useState(
         {
-            isInput: false,
-            inputData: ""
+            isInput: true,
+            inputData: "",
+            isJoining: false
         }
     )
     useEffect(() => {
@@ -27,30 +27,39 @@ export const TicTacToeRoomCreation = () => {
             console.log("✅ Connected to server with ID:", socket.id);
         });
         socket.on("joinedPlayesLists", ({ userId, gameRoomId }) => {
-            console.log({ userId, gameRoomId })
+            console.log({ userId, gameRoomId },"<------- data")
             setJoinedPlayers((prev) => ([...prev, { name: `${userId == YourId ? "(You)" : "(He)"} ${userId}  Joined`, roomId: `${gameRoomId}` }]))
         })
-        generateRandomId()
-        return () => {
-            socket.off()
-        }
+        generateRandomId() 
     }, [])
-    const joinTheGame() => {
-
+    // input change 
+    const inputChange = (e) => {
+        let { value } = e.target
+        setIputs({ ...inputs, inputData: value })
     }
-
+    // join game
+    const joinTheGame = () => {
+        socket.emit("gameJoin", { gameRoomId: inputs.inputData })
+        socket.emit("joinedPlayesLists", { userId: YourId, gameRoomId: inputs.inputData })
+    }
+    // enter the game id
+    const enterIdHandler = () => {
+        if (!loader) {
+            setIputs({ ...inputs, isJoining: true, isInput: false })
+        }
+    }
+    // create the game
     const createTheGame = () => {
         socket.emit("gameJoin", { gameRoomId: regenratedId })
         socket.emit("joinedPlayesLists", { userId: YourId, gameRoomId: regenratedId })
         setLoader(true)
     }
-    const enterIdHandler = () => {
-        setIputs({ ...inputs, isInput: !inputs.isInput })
-        
-    }
+    // random id generate
     const generateRandomId = () => {
-        let randomNumber = Math.floor(10000 + Math.random() * 90000);
-        setRegenratedId(randomNumber)
+        if (!loader) {
+            let randomNumber = Math.floor(10000 + Math.random() * 90000);
+            setRegenratedId(randomNumber)
+        }
     }
     return (
         <Container height={"100vh"} p={"4"} display={"flex"} flexDirection={"column"} gap={"9"} >
@@ -58,10 +67,7 @@ export const TicTacToeRoomCreation = () => {
             {
                 inputs.isInput
                     ?
-                    <InputGroup startElement={<FaRestroom />}>
-                        <Input placeholder="Please enter your Game ID..." />
-                    </InputGroup>
-                    : (
+                    (
                         <Clipboard.Root value={regenratedId} display={"flex"} flexDirection={"column"}>
                             <Clipboard.Label textStyle="label" alignSelf={"flex-end"}>
                                 <Link href="#" onClick={generateRandomId} color={"blue"}>
@@ -82,16 +88,14 @@ export const TicTacToeRoomCreation = () => {
                             </Clipboard.Label>
                         </Clipboard.Root>
                     )
-            }
-            {
-                inputs.isInput ?
-                    <Button loading={loader} onClick={createTheGame} loadingText={loader ? "Opponent joining..." : "Created"}> "Create Game-ID"</Button>
                     :
-                    <Button loading={loader} onClick={joinTheGame} loadingText={loader ? "Opponent joining..." : "Created"}>Join Game</Button>
+                    <InputGroup startElement={<FaRestroom />}>
+                        <Input onChange={inputChange} placeholder="Please enter your Game ID..." />
+                    </InputGroup>
             }
-            {
-                loader && <JoinedPlayers joinedPlayers={joinedPlayers} />
-            }
+            {inputs.isInput && <Button loading={loader} onClick={createTheGame} loadingText={loader ? "Opponent waiting..." : "Created"}>Create Game-ID</Button>}
+            {inputs.isJoining && <Button loading={loader} onClick={joinTheGame} loadingText={loader ?? "Joining..."}>Join Game</Button>}
+            {loader && <JoinedPlayers joinedPlayers={joinedPlayers} />}
         </Container >
     )
 }
--- server/controllers/ticTacToeController.js
+++ server/controllers/ticTacToeController.js
@@ -1,23 +1,20 @@
 exports.ticTackToeSocket = (io) => {
     io.on("connection", (socket) => {
         socket.on("gameJoin", async ({ gameRoomId }) => {
-            console.log(gameRoomId, "<--- Room joined")
-            socket.join(gameRoomId)
+            socket.join(gameRoomId) 
         })
-
         socket.on("joinedPlayesLists", async ({ userId, gameRoomId }) => {
-            console.log(userId, gameRoomId, "<--- Joined players")
+            console.log(userId, gameRoomId, "<--- userId gameRoomId")
             io.to(gameRoomId).emit("joinedPlayesLists", {
                 userId,
                 gameRoomId
             });
+            const room = io.sockets.adapter.rooms.get(gameRoomId);
+            console.log(room, "<----number of users")
         })
-
         socket.on("gameLeave", ({ gameRoomId }) => {
             socket.leave(gameRoomId);
         })
-        socket.on("disconnect", async () => {
-            console.log("socket disconnect!")
-        });
+
     });
 }
